675943f9042e8026e25edfc07c99e68186d4c5f4
Kinrany <kinrany@yandex.com>
2018-03-27 13:58:19 +0300

    fixed aspect ration

---
+++ Assets/scripts/CameraAspectRatio.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAspectRatio : MonoBehaviour {
+
+	public float AspectRatio;
+
+	void Awake() {
+		var camera = GetComponent<Camera>();
+		camera.aspect = AspectRatio;
+	}
+}
